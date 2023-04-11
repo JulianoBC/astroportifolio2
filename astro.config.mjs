1d5db0dd0c8062055b83eb3ab05110a4c9226edd
JulianoBC <88047451+JulianoBC@users.noreply.github.com>
2023-04-11 12:05:10 -0300

    edit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
+import tailwind from "tailwindcss.config"
 
 // https://astro.build/config
 export default defineConfig({
