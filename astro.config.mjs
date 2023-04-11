6180163415337c2e4fff763895a950a4df0f454b
JulianoBC <88047451+JulianoBC@users.noreply.github.com>
2023-04-11 12:37:12 -0300

    retirada de icones com @media pequeno

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
-import tailwind from "tailwindcss.config";
 
 // https://astro.build/config
 export default defineConfig({
