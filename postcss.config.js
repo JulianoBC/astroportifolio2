97c508e7c7bcf71dcc3d0ea712629dd94035e7ea
JulianoBC <88047451+JulianoBC@users.noreply.github.com>
2023-04-11 18:19:08 -0300

    teste

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,12 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+module.exports = {
+  content: ["./src/**/*.{html, js}"],
+  theme: {
+    extend: {},
   },
-}
+  plugins: [
+    {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  ],
+};
