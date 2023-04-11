82f148e3a71aae05b536bfaf257203dfc9e3cdb0
JulianoBC <88047451+JulianoBC@users.noreply.github.com>
2023-04-11 13:32:20 -0300

    modificando o build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel({
+    analytics: true,
+    includeFiles: ["./my-data.json"],
+  }),
 });
